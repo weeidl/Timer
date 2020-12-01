f789c909a72ad724237a83f2cd0d3a5f642daecc
weeidl <weeidl@list.ru>
2020-12-01 12:41:30 +0300

    Add settings

--- app/src/main/java/weeidl/com/MainActivity.java
+++ app/src/main/java/weeidl/com/MainActivity.java
@@ -2,13 +2,125 @@ package weeidl.com;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.SeekBar;
 
 public class MainActivity extends AppCompatActivity {
+
+    private SeekBar seekBar;
+    private TextView textView;
+    private boolean isTimerOn;
+    private Button button;
+    private CountDownTimer countDownTimer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        seekBar = findViewById(R.id.seekBar2);
+        textView = findViewById(R.id.textView);
+
+        seekBar.setMax(600);
+        seekBar.setProgress(30);
+        isTimerOn = false;
+
+        button = findViewById(R.id.button);
+
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+
+               long progressInMillis = progress * 1000;
+               updateTimer(progressInMillis);
+
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+    }
+
+    public void start(View view) {
+
+        if (!isTimerOn){
+            button.setText("Stop");
+            seekBar.setEnabled(false);
+            isTimerOn = true;
+
+            countDownTimer = new CountDownTimer(seekBar.getProgress() * 1000, 1000) {
+                @Override
+                public void onTick(long millisUntilFinished) {
+                    updateTimer(millisUntilFinished);
+                }
+
+                @Override
+                public void onFinish() {
+                    MediaPlayer mediaPlayer = MediaPlayer.create(getApplicationContext(), R.raw.alarm_siren_sound);
+                    mediaPlayer.start();
+                    resetTimer();
+                }
+            };
+            countDownTimer.start();
+
+        }else {
+            resetTimer();
+        }
+
+
+
+    }
+
+    private void updateTimer(long millisUntilFinished){
+        int minutes = (int) millisUntilFinished/1000/60;
+        int seconds = (int) millisUntilFinished/1000 - (minutes * 60);
+
+        String minutesString = "";
+        String secondsString = "";
+
+        if (minutes < 10){
+            minutesString = "0" + minutes;
+        }else {
+            minutesString = String.valueOf(minutes);
+        }
+
+        if (seconds < 10){
+            secondsString = "0" + seconds;
+        }else {
+            secondsString = String.valueOf(seconds);
+        }
+
+        textView.setText(minutesString + ":" + secondsString);
+    }
+
+    private void resetTimer(){
+        countDownTimer.cancel();
+        textView.setText("00:30");
+        button.setText("Start");
+        seekBar.setEnabled(true);
+        isTimerOn = false;
+        seekBar.setProgress(30);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.timer_menu, menu);
+        return true;
     }
 }
